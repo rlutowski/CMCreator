0b9190a613d52d90dcd7c401215f9f5ca6fb8c21
Rick Lutowski <rick@jreality.com>
2019-10-13 11:42:47 -0500

    Update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -4,12 +4,12 @@
 mkdir ~/jreality
 #
 #           copy distribution files to installation directory
-rm  ~/jreality/jreality-linux.tgz
-cp  jreality-linux.tgz  ~/jreality
+rm  ~/jreality/jreality-linux-v0.2.tgz
+cp  jreality-linux-v0.2.tgz  ~/jreality
 #
 #           unpack distribution files in installation directory
 cd  ~/jreality
-tar xzf jreality-linux.tgz
+tar xzf jreality-linux-v0.2.tgz
 #
 #           run the program
 ./creator.sh
