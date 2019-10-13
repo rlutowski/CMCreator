46a53b649afa56acd9c854caf9d3178fed1b5862
Rick Lutowski <rick@jreality.com>
2019-10-13 11:40:35 -0500

    Update install-win.bat

--- install-win.bat
+++ install-win.bat
@@ -4,12 +4,12 @@ REM         create installation directory
 mkdir   C:\jreality
 REM
 REM         copy distribution files to installation directory
-del     C:\jreality\jreality-win.zip 
-copy    jreality-win.zip  C:\jreality
+del     C:\jreality\jreality-win-v0.2.zip 
+copy    jreality-win-v0.2.zip  C:\jreality
 REM
 REM         unpack distribution files in installation directory
 cd      C:\jreality
-unzip   jreality-win.zip
+unzip   jreality-win-v0.2.zip
 #
 #           run the program
 creator.bat
